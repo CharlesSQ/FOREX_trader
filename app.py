b8ffb2f48fd57578bb417636372b9dd1c2fdfd00
CharlesSQ <charlesjsq@gmail.com>
2023-09-21 12:56:04 -0400

    Reset app on connection error

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from ib_insync import IB
 from trader import Trader
+from ib_manager import IBManager
 import logging
 import sys
+import asyncio
+import time
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout,
                     format="[%(asctime)s]%(levelname)s:%(message)s")
@@ -12,10 +15,12 @@ ib = IB()
 
 def main():
     # Iniciar conexión con Interactive Brokers
-    Trader.connect_ib(ib)
+    ib_manager = IBManager(ib)
+    ib_manager.connect_to_ib()
 
     # Definir el contrato (en este caso, la divisa que queremos operar: EURUSD)
     contract = Trader.define_contract('EURUSD')
+    ib_manager.contract = contract
 
     # Solicitar datos históricos de precios para el contrato desde el broker.
     # Estamos solicitando datos de los último día en barras de 5 minutos.
@@ -39,19 +44,29 @@ def main():
     # Suscribirse a las actualizaciones de datos en tiempo real
     logging.info('Subscribing to real time bars...')
     bars = ib.reqRealTimeBars(contract, 5, 'MIDPOINT', False)
+    ib_manager.requestedBars = bars
+
+    # Asignar el evento de actualización de barras a la función on_bar_update de la clase Trader
     bars.updateEvent += trader.on_bar_update
 
-    try:
-        # Iniciar el ciclo de eventos del cliente de Interactive Brokers.
-        # Esto mantendrá el programa en funcionamiento y procesará cualquier evento que venga del broker.
-        ib.run()
-    except (KeyboardInterrupt, Exception):
-        # Cancelar la suscripción cuando se presiona Ctrl+C
-        logging.info('Cancelling subscription...')
-        logging.info(f'Ordenes agregadas: {len(trader.all_orders)}')
+    # Asignar el evento de error de IB a la función handle_ib_error de la clase IBManager
+    ib.errorEvent += ib_manager.handle_ib_error
 
-        ib.disconnect()
+    # Iniciar el ciclo de eventos del cliente de Interactive Brokers.
+    logging.info('Inicio del ciclo de eventos del cliente de IB...')
+    ib.run()
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        try:
+            main()
+        except ConnectionError as e:
+            logging.error(f'Error de conexión: {e}')
+            logging.info('Reintentando la conexión en 5 segundos...')
+            ib.disconnect()
+            time.sleep(5)
+        except Exception as e:
+            logging.error(f'Error desconocido: {e}')
+            ib.disconnect()
+            sys.exit(1)
